7462dc3561458e4d9932e506843eabd00bcc1889
pavle_j <skydevhawkz@gmail.com>
2026-06-28 12:57:32 +0200

    Refactor CSS loading in functions.php to improve style management and versioning. Added checks for file existence and updated enqueueing for child theme styles, including RTL support.

--- functions.php
+++ functions.php
@@ -1,17 +1,46 @@
 <?php
 
-add_action( 'wp_enqueue_scripts', 'porto_child_css', 1001 );
+defined( 'ABSPATH' ) || exit;
 
-// Load CSS
-function porto_child_css() {
-	// porto child theme styles
-	wp_deregister_style( 'styles-child' );
-	wp_register_style( 'styles-child', esc_url( get_stylesheet_directory_uri() ) . '/style.css' );
-	wp_enqueue_style( 'styles-child' );
+add_action( 'wp_enqueue_scripts', 'porto_child_enqueue_styles', 1001 );
+
+function porto_child_enqueue_styles() {
+	$theme_dir     = get_stylesheet_directory();
+	$theme_uri     = get_stylesheet_directory_uri();
+	$theme_version = wp_get_theme( get_stylesheet() )->get( 'Version' );
+
+	$style_path = $theme_dir . '/style.css';
+
+	if ( file_exists( $style_path ) ) {
+		wp_enqueue_style(
+			'porto-child-style',
+			$theme_uri . '/style.css',
+			array(),
+			filemtime( $style_path )
+		);
+	}
+
+	$header_style_path = $theme_dir . '/assets/css/header.css';
+
+	if ( file_exists( $header_style_path ) ) {
+		wp_enqueue_style(
+			'porto-child-header',
+			$theme_uri . '/assets/css/header.css',
+			array( 'porto-child-style' ),
+			filemtime( $header_style_path )
+		);
+	}
 
 	if ( is_rtl() ) {
-		wp_deregister_style( 'styles-child-rtl' );
-		wp_register_style( 'styles-child-rtl', esc_url( get_stylesheet_directory_uri() ) . '/style_rtl.css' );
-		wp_enqueue_style( 'styles-child-rtl' );
+		$rtl_style_path = $theme_dir . '/style_rtl.css';
+
+		if ( file_exists( $rtl_style_path ) ) {
+			wp_enqueue_style(
+				'porto-child-rtl',
+				$theme_uri . '/style_rtl.css',
+				array( 'porto-child-style' ),
+				filemtime( $rtl_style_path )
+			);
+		}
 	}
 }
